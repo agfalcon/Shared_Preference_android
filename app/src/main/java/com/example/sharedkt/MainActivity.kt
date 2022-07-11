9a3a30e6776813f158f59e180786a5b11c50c563
agfal <agfalcon12@gmail.com>
2022-07-12 00:48:48 +0900

    Shared Preferences study🚀

--- app/src/main/java/com/example/sharedkt/MainActivity.kt
+++ app/src/main/java/com/example/sharedkt/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.sharedkt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        loadData()
     }
+
+    private fun loadData() {
+        val pref = getSharedPreferences("pref", 0)
+        et_hello.setText(pref.getString("name", ""))
+    }
+
+    private fun saveData() {
+        val pref = getSharedPreferences("pref", 0)
+        val edit = pref.edit()
+        edit.putString("name", et_hello.text.toString())
+        edit.apply()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        saveData()
+    }
+
+
 }
